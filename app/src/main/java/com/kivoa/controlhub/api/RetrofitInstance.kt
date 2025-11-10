18594f07d1872130f874255d03a772af26806602
Sunny Agarwal <sunny.agarwal@stablemoney.in>
2025-11-10 18:47:19 +0530

    fix image timeout

--- app/src/main/java/com/kivoa/controlhub/api/RetrofitInstance.kt
+++ app/src/main/java/com/kivoa/controlhub/api/RetrofitInstance.kt
@@ -2,8 +2,10 @@ package com.kivoa.controlhub.api
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
     //private const val BASE_URL = "https://cockroach-vercel.vercel.app/"
@@ -13,9 +15,16 @@ object RetrofitInstance {
         .add(KotlinJsonAdapterFactory())
         .build()
 
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+
     val api: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
             .create(ApiService::class.java)
--- app/src/main/java/com/kivoa/controlhub/data/ProductImage.kt
+++ app/src/main/java/com/kivoa/controlhub/data/ProductImage.kt
@@ -3,8 +3,8 @@ package com.kivoa.controlhub.data
 import com.squareup.moshi.Json
 
 data class ProductImage(
-    @Json(name = "id") val id: Int,
-    @Json(name = "product_id") val productId: Int,
+    @Json(name = "id") val id: Long,
+    @Json(name = "product_id") val productId: Long,
     @Json(name = "image_url") val imageUrl: String,
     @Json(name = "status") val status: String,
     @Json(name = "created_at") val createdAt: String,
