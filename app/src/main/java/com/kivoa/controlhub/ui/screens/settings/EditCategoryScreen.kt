1a6c7a4839aac9eb3fe24799302d6b2646368cd8
Sunny Agarwal <sunny.agarwal@stablemoney.in>
2025-11-12 22:35:18 +0530

    make tags as chips

--- app/src/main/java/com/kivoa/controlhub/ui/screens/settings/EditCategoryScreen.kt
+++ app/src/main/java/com/kivoa/controlhub/ui/screens/settings/EditCategoryScreen.kt
@@ -16,15 +16,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
@@ -34,7 +30,6 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.google.gson.Gson
 import com.kivoa.controlhub.AppBarState
 import com.kivoa.controlhub.AppBarViewModel
 import com.kivoa.controlhub.api.ApiService
@@ -46,7 +41,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class EditCategoryViewModel(application: Application, private val apiService: ApiService, private val initialCategory: ApiCategory) : ViewModel() {
+class EditCategoryViewModel(private val apiService: ApiService, private val initialCategory: ApiCategory) : ViewModel() {
 
     private val _name = MutableStateFlow(initialCategory.name)
     val name: StateFlow<String> = _name.asStateFlow()
@@ -130,7 +125,7 @@ class EditCategoryViewModelFactory(private val application: Application, private
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(EditCategoryViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return EditCategoryViewModel(application, apiService, initialCategory) as T
+            return EditCategoryViewModel(apiService, initialCategory) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
